232f471efee409f4c2ded0d01d0b82e74ab23a7f
alaaeddine <aouni.aladdine@gmail.com>
2021-05-29 23:16:02 +0200

    first commit

---
+++ cmd.js
@@ -0,0 +1,72 @@
+const yargs = require('yargs');
+const fs = require("fs");
+
+yargs.command({
+    command: 'list',
+    describe: 'Liste toutes mes notes',
+    handler: () => {
+        console.log("Voici la liste des notes");
+
+        fs.readFile("data.json", "utf-8", (err, data) => {
+            if (err) console.log(err);
+            else {
+                const notes = JSON.parse(data);
+
+                // Boucle for classique
+                // for(let i=0; i<notes.length;i++) {
+                //     console.log(`${notes[i].id}. ${notes[i].title}`);
+                // }
+
+                // foreach
+                notes.forEach(note => {
+                    console.log(`${note.id}. ${note.title}`);
+                })
+            }
+        })
+    }
+}).command({
+    command: 'add',
+    describe: "Ajoute une note",
+    builder: {
+        title: {
+            describe: "Titre de ma note",
+            demandOption: true,
+            type: "string"
+        },
+        message: {
+            describe: "Message de ma note",
+            demandOption: false,
+            type: "string"
+        }
+    },
+    handler: (argv) => {
+        const newNote = [{
+            title: argv.title,
+            message: argv.message
+        }]
+
+        const newNoteJSON = JSON.stringify(newNote);
+        fs.writeFile("data.json", newNoteJSON, (err) => {
+            if (err) console.log(err);
+            else {
+                console.log("La nouvelle note a été sauvegardée");
+            }
+        });
+    }
+}).command({
+    command: 'remove',
+    describe: "Supprime une note",
+    handler: (argv) => {
+        const newNote = [{
+            title: argv.title,
+            message: argv.message
+        }]
+        console.log("Chaud pour supprimer une note");
+    }
+}).command({
+    command: 'read',
+    describe: "Affiche le détail d'une note",
+    handler: () => {
+        console.log("Voici le détail d'une note");
+    }
+}).argv;
